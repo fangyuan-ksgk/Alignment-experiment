552e8ae83d29ae526628fda65dc577ac840e8b79
FangyuanYu <fangyuan.yu@gmail.com>
2024-05-01 09:44:07 +0800

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,4 @@
+pip install  --upgrade transformers datasets accelerate evaluate bitsandbytes trl peft torch
+pip install ninja packaging tensorboardX tensorboard
+pip install flash-attn
+export HF_TOKEN = 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
