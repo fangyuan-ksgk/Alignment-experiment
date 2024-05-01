b6bb15be077c2dc2931a97e1daa883efb11201da
FangyuanYu <fangyuan.yu@gmail.com>
2024-05-01 18:01:22 +0800

    uses ReFT to nail the stuff

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
 pip install  --upgrade transformers datasets accelerate evaluate bitsandbytes trl peft torch
 pip install ninja packaging tensorboardX tensorboard
 pip install flash-attn
-export HF_TOKEN = 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
+pip install -U autotrain-advanced
+pip install "unsloth[colab-new] @ git+https://github.com/unslothai/unsloth.git"
+pip install --no-deps "xformers<0.0.26" trl peft accelerate bitsandbytes
+pip install git+https://github.com/stanfordnlp/pyreft.git
+pip install dataclasses
+export HF_TOKEN=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
